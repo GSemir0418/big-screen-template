bed7534022990c28683c9d95c110d785a8602050
GSemir <845217811@qq.com>
2022-04-27 16:32:05 +0800

    feat: 完成col2，chart2、5

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -11,11 +11,16 @@ import lineUp from '../../../assets/img/lineUp.png'
 import lineDown from '../../../assets/img/lineDown.png'
 import borderUp from '../../../assets/img/borderUp.png'
 import borderDown from '../../../assets/img/borderDown.png'
-import useHighCharts3d from '../../hooks/useHighCharts3d'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import HighCharts3d from 'highcharts/highcharts-3d'
+HighCharts3d(Highcharts)
 const Home = () => {
   const chartRef = useRef<HTMLDivElement | null>(null)
   const chart1Ref = useRef<HTMLDivElement | null>(null)
+  const chart2Ref = useRef<HTMLDivElement | null>(null)
   const chart3Ref = useRef<HTMLDivElement | null>(null)
+
   const option1 = {
     color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
     title: {
@@ -119,6 +124,50 @@ const Home = () => {
     ],
   }
   useEcharts(chart1Ref, option1)
+  const option2 = {
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      top: '20%',
+      containLabel: true,
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985',
+        },
+      },
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: { show: false },
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+          color: 'rgba(180, 180, 180, 0.2)',
+        },
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 1, color: '#071e3f' },
+            { offset: 0, color: '#1ECED7' },
+          ]),
+        },
+      },
+    ],
+  }
+  useEcharts(chart2Ref, option2)
+
   const option3 = {
     grid: {
       left: '3%',
@@ -235,10 +284,10 @@ const Home = () => {
         roam: false,
         // 视角缩放比例稍大些
         zoom: 1.02,
-        top: pxToRem(150),
-        left: pxToRem(250),
+        top: pxToRem(30),
+        left: pxToRem(240),
         // 视野范围，模拟透视角度
-        aspectScale: 1,
+        aspectScale: 1.2,
         label: {
           normal: {
             show: false,
@@ -249,7 +298,7 @@ const Home = () => {
             // areaColor: '#CDDCEF',
             areaColor: '#082243',
             borderColor: '#247094',
-            borderWidth: 1,
+            borderWidth: 2,
             borderType: 'solid',
             opacity: 0.6,
             shadowBlur: 1,
@@ -267,7 +316,7 @@ const Home = () => {
         {
           type: 'map',
           geoIndex: 1,
-          aspectScale: 1,
+          aspectScale: 1.2,
           map: 'CN',
           roam: false,
           nameProperty: 'name',
@@ -287,8 +336,8 @@ const Home = () => {
               opacity: 0.6,
               areaColor: '#082243',
               borderColor: '#39B3ED',
-              borderWidth: '1',
-              shadowBlur: 1,
+              borderWidth: 2,
+              shadowBlur: 2,
               shadowColor: 'black',
               shadowOffsetX: 5,
               shadowOffsetY: 5,
@@ -319,27 +368,6 @@ const Home = () => {
           },
         },
       ],
-      // series: [
-      //   {
-      //     type: 'map',
-      //     mapType: 'CN',
-      //     data: [{ name: '赣州市', value: 1 }],
-      //     label: { show: false, color: 'white' },
-      //     itemStyle: {
-      //       opacity: 0.5,
-      //       areaColor: '#082243',
-      //       shadowBlur: 1,
-      //       shadowColor: '#39B3ED',
-      //       shadowOffsetX: 10,
-      //       shadowOffsetY: -1,
-      //       borderColor: '#39B3ED',
-      //       emphasis: {
-      //         label: { color: 'white', fontSize: '1rem' },
-      //         areaColor: '#59162D',
-      //       },
-      //     },
-      //   },
-      // ],
     })
   })
   const options3d = {
@@ -368,25 +396,27 @@ const Home = () => {
     },
     series: [
       {
+        type: 'pie',
         name: '货物金额',
         dataLabels: { enabled: false },
         data: [
           {
             name: 'test1',
             y: 30,
+            h: 30,
             sliced: true,
             color: 'red',
           },
           {
             name: 'others',
             y: 70,
+            h: 90,
             color: 'white',
           },
         ],
       },
     ],
   }
-  const chart3d = useHighCharts3d(options3d)
   return (
     <div className="home">
       <header style={{ backgroundImage: `url(${headerBg})` }} />
@@ -399,7 +429,13 @@ const Home = () => {
             </div>
             <div ref={chart1Ref} style={{ height: '100%' }} />
           </div>
-          <div className="chart"></div>
+          <div className="chart">
+            <div className="chart-title">
+              <img className="chart-title-icon" src={titleIcon} />
+              Chart2 Title
+            </div>
+            <div ref={chart2Ref} style={{ height: '100%' }} />
+          </div>
           <div className="chart">
             <div className="chart-title">
               <img className="chart-title-icon" src={titleIcon} />
@@ -424,40 +460,186 @@ const Home = () => {
         <section className="col2">
           <div
             ref={chartRef}
+            className="map"
             style={{
-              height: '100%',
               transform: `
-              translate(${pxToRem(0)}px,${pxToRem(-180)}px)
+              translate(${pxToRem(0)}px,${pxToRem(-50)}px)
               `,
-              // transform: `
-              // rotate(30deg)
-              //   rotate(70deg)
-              //   rotateY(50deg)
-              //   translate(${pxToRem(-350)}px,${pxToRem(-90)}px)
-              // `,
             }}
           />
+          <div className="table">
+            <div className="chart-title">
+              <img className="chart-title-icon" src={titleIcon} />
+              Table Title
+            </div>
+            <div className="table-header">
+              <span className="table-header-content">类型</span>
+              <span className="table-header-content">计划开始时间</span>
+              <span className="table-header-content">计划结束时间</span>
+              <span className="table-header-content">发布人</span>
+              <span className="table-header-content">接受人</span>
+              <span className="table-header-content">状态</span>
+            </div>
+            <div className="table-body">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>生产计划</td>
+                    <td>2022-02</td>
+                    <td>2022-04</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                  </tr>
+                  <tr>
+                    <td>生产计划</td>
+                    <td>2022-02</td>
+                    <td>2022-04</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                  </tr>
+                  <tr>
+                    <td>生产计划</td>
+                    <td>2022-02</td>
+                    <td>2022-04</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                  </tr>
+                  <tr>
+                    <td>生产计划</td>
+                    <td>2022-02</td>
+                    <td>2022-04</td>
+                    <td>123</td>
+                    <td>123</td>
+                    <td>123</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="footer">
+            <span className="footer-1">页面1</span>
+            <span className="footer-2">页面2</span>
+            <span className="footer-home">首页</span>
+            <span className="footer-3">页面3</span>
+            <span className="footer-4">页面4</span>
+          </div>
         </section>
         <section className="col3">
-          <div className="chart"></div>
+          <div className="chart">
+            <div className="chart-title">
+              <img className="chart-title-icon" src={titleIcon} />
+              Chart5 Title
+            </div>
+            <div className="chart5-warpper">
+              <div className="chart5-1">
+                <div className="chartIcon" />
+                <div className="chart5-1-data-wrapper">
+                  <div style={{ fontSize: `${pxToRem(14)}px`, color: 'white' }}>
+                    数据指标
+                  </div>
+                  <div
+                    style={{
+                      fontFamily: 'Impact',
+                      fontSize: `${pxToRem(18)}px`,
+                      color: '#4CF5FA',
+                    }}
+                  >
+                    125632
+                  </div>
+                  <div
+                    style={{ fontSize: `${pxToRem(12)}px`, color: '#FF7173' }}
+                  >
+                    环比 +25%
+                  </div>
+                </div>
+              </div>
+              <div className="chart5-2">
+                <div className="chartIcon" />
+                <div className="chart5-1-data-wrapper">
+                  <div style={{ fontSize: `${pxToRem(14)}px`, color: 'white' }}>
+                    数据指标
+                  </div>
+                  <div
+                    style={{
+                      fontFamily: 'Impact',
+                      fontSize: `${pxToRem(18)}px`,
+                      color: '#4CF5FA',
+                    }}
+                  >
+                    125632
+                  </div>
+                  <div
+                    style={{ fontSize: `${pxToRem(12)}px`, color: '#FF7173' }}
+                  >
+                    环比 +25%
+                  </div>
+                </div>
+              </div>
+              <div className="chart5-3">
+                <div className="chartIcon" />
+                <div className="chart5-1-data-wrapper">
+                  <div style={{ fontSize: `${pxToRem(14)}px`, color: 'white' }}>
+                    数据指标
+                  </div>
+                  <div
+                    style={{
+                      fontFamily: 'Impact',
+                      fontSize: `${pxToRem(18)}px`,
+                      color: '#4CF5FA',
+                    }}
+                  >
+                    125632
+                  </div>
+                  <div
+                    style={{ fontSize: `${pxToRem(12)}px`, color: '#FF7173' }}
+                  >
+                    环比 +25%
+                  </div>
+                </div>
+              </div>
+              <div className="chart5-4">
+                <div className="chartIcon" />
+                <div className="chart5-1-data-wrapper">
+                  <div style={{ fontSize: `${pxToRem(14)}px`, color: 'white' }}>
+                    数据指标
+                  </div>
+                  <div
+                    style={{
+                      fontFamily: 'Impact',
+                      fontSize: `${pxToRem(18)}px`,
+                      color: '#4CF5FA',
+                    }}
+                  >
+                    125632
+                  </div>
+                  <div
+                    style={{ fontSize: `${pxToRem(12)}px`, color: '#FF7173' }}
+                  >
+                    环比 +25%
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="chart">
             <div className="chart-title">
               <img className="chart-title-icon" src={titleIcon} />
               Chart6 Title
             </div>
-            <div
+            {/* <div
+              className="chart-pie3d"
               style={{
-                background: 'red',
-                width: '100%',
-                height: '100%',
+                background: 'lightblue',
                 display: 'flex',
                 flexDirection: 'row',
                 justifyContent: 'center',
               }}
             >
-              <div>{chart3d}</div>
-              <div>{chart3d}</div>
-            </div>
+              <HighchartsReact highcharts={Highcharts} options={options3d} />
+            </div> */}
           </div>
           <div className="chart"></div>
           <div className="chart4"></div>
