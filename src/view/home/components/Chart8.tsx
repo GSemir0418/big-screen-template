57f47d9ec0e01dd4b89bb6f89a08075258db2f8f
GSemir <845217811@qq.com>
2022-05-06 17:43:35 +0800

    fix: 实现数字定时滚动特效

--- src/view/home/components/Chart8.tsx
+++ src/view/home/components/Chart8.tsx
@@ -1,12 +1,35 @@
 import ChartWrapper from './ChartWrapper'
-import { useCountUp } from 'react-countup'
+import CountUp, { useCountUp } from 'react-countup'
+import React, { useState, useEffect, useRef } from 'react'
 const Chart8 = () => {
-  useCountUp({ ref: 'counter1', end: 1234567, duration: 4 })
+  const currentNum = useRef(0)
+  const newNum = useRef(0)
+  // eslint-disable-next-line prefer-destructuring
+  const setNum = useState(newNum.current)[1]
+  useEffect(() => {
+    const t = setInterval(() => {
+      currentNum.current = newNum.current
+      newNum.current += 111
+      setNum(newNum.current)
+    }, 2000)
+    return () => {
+      clearInterval(t)
+      currentNum.current = 0
+      newNum.current = 0
+    }
+  }, [])
   useCountUp({ ref: 'counter2', end: 1234, duration: 3 })
   return (
-    <ChartWrapper chartClassName="chart4" title="Chart7 Title">
+    <ChartWrapper chartClassName="chart4" title="Chart8 Title">
       <>
-        <div id="counter1" />
+        <CountUp start={currentNum.current} end={newNum.current} duration={1}>
+          {({ countUpRef }) => (
+            <div
+              id="counter1"
+              ref={countUpRef as React.RefObject<HTMLDivElement>}
+            />
+          )}
+        </CountUp>
         <div id="counter2" />
       </>
     </ChartWrapper>
